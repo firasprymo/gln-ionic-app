bf1ec183823b9d0ad150a0721b66a365d0658351
firas abdennaji <f.abdennaji@numeryx.fr>
2021-12-12 05:54:33 +0100

    confirmation

--- src/app/pages/confirmation/confirmation.page.ts
+++ src/app/pages/confirmation/confirmation.page.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfirmationPage implements OnInit {
 
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400,
+    slidesPerView: 3,
+    slidesPerGroup: 1,
+    watchSlidesProgress: true,
+    spaceBetween: 10,
+    scrollbar: { draggable: true },
+    loop: true
+    // virtualTranslate: true,
+  };
   constructor() { }
 
   ngOnInit() {
